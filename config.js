af9422c5eaf82f919191823af2c108c83232d294
keeize <107405609+qeize@users.noreply.github.com>
2024-11-30 10:25:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    owner: ['6285732351432@s.whatsapp.net'], // make format nomormu@s.whatsapp.net
+    owner: ['6285252689486@s.whatsapp.net'], // make format nomormu@s.whatsapp.net
 };
 
 module.exports = config;
